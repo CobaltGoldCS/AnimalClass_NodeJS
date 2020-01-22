8a5343d58862b9d9b46ff67b630b5b9e5a2a2919
CobaltGoldCS <cobaltgoldcsprinks@gmail.com>
2020-01-22 20:55:10 +0000

    Added mushroom subclass

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+util = require("util")
 class Animal{
   constructor(faveFood, energy, attkpower, name){
     this.faveFood = faveFood;
@@ -10,17 +11,20 @@ class Animal{
     if (!this.dead){
       if (foodtype == this.faveFood){
       console.log("Nom");
-      this.energy+= 10*numFood
+      return  this.energy+= 10*numFood;
+      }
+      else{
+        return console.log(this.name, "didnt like that")
       }
     }
     else{
-      console.log(this.name, "is dead")
+      return console.log(this.name, "is dead");
     }
     
 
   }
   check(){
-    let info = [this.energy, this.attkpower]
+    let info = [this.energy, this.attkpower];
     if (this.energy <= 0){
       this.dead = true;
     }
@@ -28,20 +32,44 @@ class Animal{
   }
   fight(otherAnimal){
     if (!this.dead){
-      otherAnimal.energy-= this.energy * this.attkpower
-      otherAnimal.check()
+      otherAnimal.energy-= this.energy * this.attkpower;
+      otherAnimal.check();
       if (otherAnimal.dead == true){
-        console.log(otherAnimal.name, "is dead")
+        return console.log(otherAnimal.name, "is dead");
       }
     }
     else{
-      console.log(this.name, "is dead")
+      return console.log(this.name, "is dead");
     }
     
   }
 }
-chinchilla = new Animal("Grass", 1000, 10, "Jerry");
-chinchilla.eat(100, "Grass");
-wolf = new Animal("chinchilla", 1000, 10, "Dax");
-wolf.fight(chinchilla);
-chinchilla.fight(wolf);
+
+class Mushroom extends Animal{
+  constructor(faveFood="Dead Plants", energy, attkpower, name) {
+    super(faveFood, energy, attkpower, name);
+    this.maturity = 0
+  }
+  fight(otherAnimal){
+    super.fight(otherAnimal);
+  }
+  spore(num){
+    for (num > 0; num--;){
+      this.energy -= 10;
+      this.clone();
+    }
+  }
+  clone(){
+    this.attkpower += 10;
+  }
+  grow(){
+    this.energy -= 100;
+    this.maturity++;
+  }
+  eat(numFood, foodtype){
+    super.eat(numFood, foodtype)
+  }
+}
+foo = new Mushroom(faveFood = "Dead Plants", energy = 1000, attkpower = 10, name ="Foo")
+foo.eat(100, "Dead Plants")
+console.log(foo.check())
